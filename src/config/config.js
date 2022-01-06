2fda016167f77016238cee782ab0f36b3b8b76dd
beotrera <otrera996@gmail.com>
2022-01-06 11:24:26 -0300

    add ts

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-const SERVER_MODE_FROM_ARGS = process.argv[2];
-
-export default  {
-  CLUSTER: SERVER_MODE_FROM_ARGS === "CLUSTER" ? true : false,
-};
--- src/lib/auth/index.js
+++
@@ -1,9 +0,0 @@
-const isAuth =(req,res,next)=>{
-    if (req.isAuthenticated()) {
-        next();
-      } else {
-        res.status(403).render("login");
-      }
-}
-
-export default isAuth
