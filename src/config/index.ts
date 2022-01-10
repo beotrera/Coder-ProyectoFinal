70829e7e56db3378d8dbbd0a2c2c06413c6ca22b
beotrera <otrera996@gmail.com>
2022-01-10 11:16:35 -0300

    change port

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { APP_PORT,NODE_ENV,MONGO_DB_URI, JWT_SECRET } = process.env
+const { PORT,NODE_ENV,MONGO_DB_URI, JWT_SECRET } = process.env
 
 export default {
     NODE_ENV, 
-    APP_PORT,
+    APP_PORT:PORT,
     MONGO_DB_URI,
     JWT_SECRET
 }
